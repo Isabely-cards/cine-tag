b0fec5a3fb0acef573cb07e007d0489fb9c2abc1
Isabely Cardoso Figueiredo <isabelycrds@gmail.com>
2023-10-28 19:21:17 -0300

    Create index.jsx

---
+++ src/pages/NãoEncontrada/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css"
+
+function NãoEncontrada () {
+    return(
+        <section className={styles.container}>
+
+            <h2>Ops!</h2>
+            <p>O conteúdo que você procura não foi encontrado!</p>
+
+        </section>
+    )
+}
+export default NãoEncontrada
