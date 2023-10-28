d4be55d59fc160280327f3a41f7b3765503e742b
Isabely Cardoso Figueiredo <isabelycrds@gmail.com>
2023-10-28 19:21:33 -0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,26 +1,21 @@
-import Cabecalho from "./components/Cabecalho";
 import Favoritos from "./pages/Favoritos";
 import Inicio from "./pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Rodape from "./components/Rodape";
-import Container from "./components/Container";
-import FavoritosProvider from "./contextos/Favoritos";
 import Player from "./pages/Player";
+import NãoEncontrada from "./pages/NãoEncontrada";
+import PaginaBase from "./pages/PaginaBase";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Cabecalho />
-            <Container>
-                <FavoritosProvider>
-                    <Routes>
-                        <Route path="/" element={<Inicio />}></Route>
-                        <Route path="/favoritos" element={<Favoritos />}></Route>
-                        <Route path="/:id" element={<Player />}></Route>
-                    </Routes>
-                </FavoritosProvider>
-            </Container>
-            <Rodape />
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />}></Route>
+                    <Route path="favoritos" element={<Favoritos />}></Route>
+                    <Route path=":id" element={<Player />}></Route>
+                    <Route path="*" element={<NãoEncontrada />}></Route>
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
