cf1458055d6a6665965df2e0d361cad569e89cf4
Isabely Cardoso Figueiredo <isabelycrds@gmail.com>
2023-10-29 11:04:38 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import AppRoutes from './routes.jsx'
 
-                   
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppRoutes/>
